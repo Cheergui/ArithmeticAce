17afe3e479f3db92e96ff9d2fd8f1ec92facfe3d
Cheergui <aminejelloul23@gmail.com>
2023-05-11 01:02:36 +0200

    first commit

---
+++ exercise.py
@@ -0,0 +1,14 @@
+
+class CalculationGenerator:
+    #def __init__(self) -> None:
+    def __init__(self, num_type, num_digits, operation, difficulty):
+        self.num_type = num_type
+        self.num_digits = num_digits
+        self.operation = operation
+        self.difficulty = difficulty
+
+    
+
+test = CalculationGenerator()
+print(test)
+
